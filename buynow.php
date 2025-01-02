3bb40f92504c0daec2f6b4993d83ac99accad715
Sandun Senarath <139609887+SandunSenarath@users.noreply.github.com>
2025-01-02 16:34:06 +0000

    add model tecxt feild to buynow page

--- buynow.php
+++ buynow.php
@@ -84,14 +84,14 @@
                   <div class="row">
                     <div class="col-50">
                       <h3>Billing Address</h3>
+                      <label for="mdl"><i class="fa fa-model-card-o"></i> Model</label>
+                      <input type="text" id="model" name="model" placeholder="SAMSUNG A55">
                       <label for="fname"><i class="fa fa-user"></i> Full Name</label>
                       <input type="text" id="fname" name="firstname" placeholder="Your Name">
                       <label for="email"><i class="fa fa-envelope"></i> Email</label>
                       <input type="text" id="email" name="email" placeholder="youremail@gmail.com">
                       <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
                       <input type="text" id="adr" name="address" placeholder="542 , Street">
-                      <label for="mdl"><i class="fa fa-model-card-o"></i> Model</label>
-                      <input type="text" id="model" name="model" placeholder="SAMSUNG A55">
           
                       <div class="row">
                         <div class="col-50">
