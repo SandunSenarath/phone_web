6df01936712cb55e44d62dbe548a5e0cd0d2cb3d
Sandun Senarath <139609887+SandunSenarath@users.noreply.github.com>
2025-01-16 20:42:03 +0000

    updated iphone page

--- models_iphone.php
+++ models_iphone.php
@@ -133,34 +133,49 @@ Other features: 5G connectivity, water and dust resistance</p>
 <div class="row row-cols-1 row-cols-md-3 g-4">
   <div class="col">
     <div class="products card h-100">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="Img/Apple-iPhone13.jpg" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h5 class="card-title">iPhone 13</h5>
+        <p class="card-text">Display: 6.1-inch OLED Retina XDR display   
+Processor: A15 Bionic chip   
+Camera: Dual 12MP cameras   
+Battery: All-day battery life   
+Security: Emergency SOS via satellite   
+Other features: 5G connectivity, water and dust resistance</p>
       </div>
       <div class="card-footer">
-        <a href="#" class="btn btn-primary">Buy Now</a>
+        <a href="buynow.php" class="btn btn-primary">Buy Now</a>
       </div>
     </div>
   </div>
   <div class="col">
     <div class="products card h-100">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="Img/Apple-iPhone13-Pro.jpg" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        <h5 class="card-title">iPhone 13 Pro</h5>
+        <p class="card-text">Display: 6.1-inch Super Retina XDR display with ProMotion (120Hz)<br>
+Processor: A15 Bionic chip with 5-core GPU<br>
+Camera: Triple 12MP cameras (Wide, Ultra-Wide, Telephoto) with 3x optical zoom<br>
+Battery: Up to 22 hours video playback<br>
+Security: Face ID, Emergency SOS via satellite<br>
+Other features: 5G connectivity, ProRAW & ProRes video, <br>MagSafe, Water & dust resistance (IP68</p>
       </div>
       <div class="card-footer">
-        <a href="#" class="btn btn-primary">Buy Now</a>
+        <a href="buynow.php" class="btn btn-primary">Buy Now</a>
       </div>
     </div>
   </div>
   <div class="col">
     <div class="products card h-100">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="Img/Apple-iPhone-13-Pro-Max.jpg" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        <h5 class="card-title">iPhone 13 Pro Max</h5>
+        <p class="card-text">Display: 6.7-inch Super Retina XDR display with ProMotion (120Hz)<br>
+Processor: A15 Bionic chip with 5-core GPU<br>
+Camera: Triple 12MP cameras (Wide, Ultra-Wide, Telephoto) with 3x optical zoom and Night Mode<br>
+Battery: Up to 28 hours video playback<br>
+Security: Face ID, Emergency SOS via satellite<br>
+Other features: 5G connectivity, ProRAW & ProRes video, MagSafe,<br> Water & dust resistance (IP68), 1TB storage option</p>
       </div>
       <div class="card-footer">
         <a href="#" class="btn btn-primary">Buy Now</a>
@@ -174,37 +189,52 @@ Other features: 5G connectivity, water and dust resistance</p>
   <div class="row row-cols-1 row-cols-md-3 g-4">
   <div class="col">
     <div class="products card h-100">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="Img/iphone-12.jpg" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h5 class="card-title">iPhone 12</h5>
+        <p class="card-text">Display: 6.1-inch Super Retina XDR display (OLED)<br>
+Processor: A14 Bionic chip with 6-core CPU and 4-core GPU<br>
+Camera: Dual 12MP cameras (Wide and Ultra-Wide) with Night Mode<br>
+Battery: Up to 17 hours video playback<br>
+Security: Face ID<br>
+Other features: 5G connectivity, MagSafe, Water & dust resistance (IP68)</p>
       </div>
       <div class="card-footer">
-        <a href="#" class="btn btn-primary">Buy Now</a>
+        <a href="buynow.php" class="btn btn-primary">Buy Now</a>
       </div>
     </div>
   </div>
   <div class="col">
     <div class="products card h-100">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="Img/iphone-12-pro.jpg" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        <h5 class="card-title">iPhone 12 Pro</h5>
+        <p class="card-text">Display: 6.1-inch Super Retina XDR display (OLED)<br>
+Processor: A14 Bionic chip with 6-core CPU and 4-core GPU<br>
+Camera: Triple 12MP cameras (Wide, Ultra-Wide, Telephoto) with 4x optical zoom, <br>Night Mode, and LiDAR scanner<br>
+Battery: Up to 17 hours video playback<br>
+Security: Face ID<br>
+Other features: 5G connectivity, MagSafe, Water & dust resistance (IP68), ProRAW photography</p>
       </div>
       <div class="card-footer">
-        <a href="#" class="btn btn-primary">Buy Now</a>
+        <a href="buynow.php" class="btn btn-primary">Buy Now</a>
       </div>
     </div>
   </div>
   <div class="col">
     <div class="products card h-100">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="Img/12-pro-max.jpg" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        <h5 class="card-title">iPhone 12 Pro Max</h5>
+        <p class="card-text">Display: 6.7-inch Super Retina XDR display (OLED)<br>
+Processor: A14 Bionic chip with 6-core CPU and 4-core GPU<br>
+Camera: Triple 12MP cameras (Wide, Ultra-Wide, Telephoto) with 5x optical zoom,<br> Night Mode, and LiDAR scanner<br>
+Battery: Up to 20 hours video playback<br>
+Security: Face ID<br>
+Other features: 5G connectivity, MagSafe, Water & dust resistance (IP68), ProRAW photography</p>
       </div>
       <div class="card-footer">
-        <a href="#" class="btn btn-primary">Buy Now</a>
+        <a href="buynow.php" class="btn btn-primary">Buy Now</a>
       </div>
     </div>
   </div>
@@ -213,37 +243,52 @@ Other features: 5G connectivity, water and dust resistance</p>
 <div class="row row-cols-1 row-cols-md-3 g-4">
   <div class="col">
     <div class="products card h-100">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="Img/iphone-11.jpg" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h5 class="card-title">iPhone 11</h5>
+        <p class="card-text">Display: 6.1-inch Liquid Retina HD display (LCD)<br>
+Processor: A13 Bionic chip with 6-core CPU and 4-core GPU<br>
+Camera: Dual 12MP cameras (Wide and Ultra-Wide) with Night Mode and 4K video recording<br>
+Battery: Up to 17 hours video playback<br>
+Security: Face ID<br>
+Other features: 4G LTE, Water & dust resistance (IP68), Ultra-Wideband chip, Fast charging support</p>
       </div>
       <div class="card-footer">
-        <a href="#" class="btn btn-primary">Buy Now</a>
+        <a href="buynow.php" class="btn btn-primary">Buy Now</a>
       </div>
     </div>
   </div>
   <div class="col">
     <div class="products card h-100">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="Img/iphone11-pro.jpg" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        <h5 class="card-title">iPhone 11 Pro</h5>
+        <p class="card-text">Display: 5.8-inch Super Retina XDR display (OLED)<br>
+Processor: A13 Bionic chip with 6-core CPU and 4-core GPU<br>
+Camera: Triple 12MP cameras (Wide, Ultra-Wide, Telephoto) with Night Mode and 4K video recording<br>
+Battery: Up to 18 hours video playback<br>
+Security: Face ID<br>
+Other features: 4G LTE, Water & dust resistance (IP68), MagSafe, Fast charging support</p>
       </div>
       <div class="card-footer">
-        <a href="#" class="btn btn-primary">Buy Now</a>
+        <a href="buynow.php" class="btn btn-primary">Buy Now</a>
       </div>
     </div>
   </div>
   <div class="col">
     <div class="products card h-100">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="Img/Apple_iPhone-11-Pro-max.jpg" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        <h5 class="card-title">iPhone 11 Pro Max</h5>
+        <p class="card-text">Display: 6.5-inch Super Retina XDR display (OLED)<br>
+Processor: A13 Bionic chip with 6-core CPU and 4-core GPU<br>
+Camera: Triple 12MP cameras (Wide, Ultra-Wide, Telephoto) with Night Mode,<br> 4K video recording, and up to 2x optical zoom<br>
+Battery: Up to 20 hours video playback<br>
+Security: Face ID<br>
+Other features: 4G LTE, Water & dust resistance (IP68), MagSafe, Fast charging support</p>
       </div>
       <div class="card-footer">
-        <a href="#" class="btn btn-primary">Buy Now</a>
+        <a href="buynow.php" class="btn btn-primary">Buy Now</a>
       </div>
     </div>
   </div>
