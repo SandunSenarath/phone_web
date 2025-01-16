c98a9afed260bc2d8f499f43d7af550f2256d9ff
SandunSenarath <sandunsenarath75@gmail.com>
2025-01-16 14:16:59 +0530

    create my function page

---
+++ addcard.php
@@ -0,0 +1,35 @@
+<?php
+try {
+  require_once "public/dbconnect.php";
+  require_once "public/myfunc.php";
+  if (isset($_GET['types'])) {
+    $tids=$_GET['types'];
+    //print_r($tids);
+    $items = FilterBooksByTypes($connect,$tids);
+  } else {
+    $items = GetBookCoverPrice($connect);
+  }
+  
+  
+  foreach ( $books as $key => $value) {
+    $image = $value['image_name'];
+    $price = $value['price'];
+    $name = $value['name'];
+    $description=$value['description'];
+?>
+
+<div class="card float-start" style="width: 12rem; margin: 5px;">
+  <img class="card-img-top img-thumbnail" src="Img/<?php echo $image; ?>" class="card-img-top" alt="<?php echo $name; ?>">
+  <div class="card-body">
+    <p class="card-text">Price:<?php echo $price; ?></p>
+    <a href="#" class="btn btn-primary">Buy </a>
+  </div>
+</div>
+
+<?php
+  } //loop close
+}//try close
+  catch (e) {
+    echo "<h1>500 Internal error</h1>";
+  }
+?>
---
+++ public/myfunc.php
@@ -0,0 +1,43 @@
+<?php 
+
+function GetData($connect,$query){
+	$arr = array();
+	try {
+		$sql = $query;
+		$result = mysqli_query($connect,$sql);	
+		while ($row = mysqli_fetch_assoc($result)) {
+			array_push($arr,$row);
+		}
+	} catch (Exception $e) {
+		$arr = null;
+		die($e->getMessage());
+	}
+	return $arr;
+}
+
+function GetBookCoverPrice($connect)
+{
+	$sql ="select Book_name,image_name,price from books";
+	return GetData($connect,$sql);
+}
+function GetBookTypes($connect)
+{
+	$sql ="select * from booktype";
+	return GetData($connect,$sql);
+}
+
+function FilterBooksByTypes($connect,$tids)
+{
+	$sql ="select * from books where ";
+	foreach ($tids as $key => $tid) {
+		if ($key == 0) {
+			$sql.= "Book_type_id = '$tid'";
+		}else{
+			$sql.= " or Book_type_id = '$tid'";
+		}
+	}
+	return GetData($connect,$sql);
+}
+
+
+?>
