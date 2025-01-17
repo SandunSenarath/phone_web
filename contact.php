94227ae4fe7bad489fe197c1a38dde1e498cf155
Sandun Senarath <139609887+SandunSenarath@users.noreply.github.com>
2025-01-17 08:24:29 +0000

    sign in page updated

--- contact.php
+++ contact.php
@@ -48,7 +48,7 @@
                     <a class="nav-link" href="contact.php">Contact Us</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="singup page.php">Sing Up</a>
+                    <a class="nav-link" href="signup_login/login.php">Sign In</a>
                   </li>
                   <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
